93c8b0546ce0d00523f6a46ed2cb6f88cc57c56d
vKxni <Konradtoesch@icloud.com>
2026-03-18 17:04:28 +0100

    fix format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod cli;
+mod cli_commands;
+mod datadriver;
 mod entry;
 mod explorer;
 mod scanner;
 mod utils;
 mod writer;
-mod datadriver;
-mod cli_commands;
 
 fn main() {
     datadriver::run();
